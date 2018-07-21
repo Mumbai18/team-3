ada65ed456b2125f53ebdba20be1e71956a2cabd
dhaneshm064 <33578985+dhaneshm064@users.noreply.github.com>
2018-07-21 17:17:27 +0530

    registeruser

--- startbootstrap-sb-admin-gh-pages/registeruser.php
+++ startbootstrap-sb-admin-gh-pages/registeruser.php
@@ -14,21 +14,21 @@
             <div class="form-row">
               <div class="col-md-6">
                 <label for="exampleInputName">First name</label>
-                <input class="form-control"name="FirstName" id="exampleInputName" type="text" aria-describedby="nameHelp" placeholder="Enter first name">
+                <input class="form-control"name="firstname" id="exampleInputName" type="text" aria-describedby="nameHelp" placeholder="Enter first name">
               </div>
               <div class="col-md-6">
                 <label for="exampleInputLastName">Last name</label>
-                <input class="form-control" name="LastName" id="exampleInputLastName" type="text" aria-describedby="nameHelp" placeholder="Enter last name">
+                <input class="form-control" name="lastname" id="exampleInputLastName" type="text" aria-describedby="nameHelp" placeholder="Enter last name">
               </div>
+              <br>
               <div class="btn-group">
-    <button type="button" class="form-control btn btn-default dropdown-toggle" data-toggle="dropdown">
-        Select Role<span class="caret"></span>
-    </button>
-    <ul class="dropdown-menu" role="menu">
-        <li><a href="#">Donor</a></li>
-        <li><a href="#">Volunteer</a></li>
-        <li><a href="#">CoreCommittee</a></li>
-    </ul>
+                <select name="Role">
+              <option value="None">None</option>
+              <option value="Donor">Donor</option>
+              <option value="Volunteer">Volunteer</option>
+              <option value="Committee">Committee</option>
+              <option value="Core-Committee">Core-Committee</option>
+              </select>
 </div>
 <br><br>
 
@@ -46,11 +46,11 @@
             <div class="form-row">
               <div class="col-md-6">
                 <label for="exampleInputPassword1">Password</label>
-                <input class="form-control" id="exampleInputPassword1" type="password" placeholder="Password">
+                <input class="form-control" id="exampleInputPassword1" name="password" type="password" placeholder="Password">
               </div>
               <div class="col-md-6">
                 <label for="exampleConfirmPassword">Confirm password</label>
-                <input class="form-control" id="exampleConfirmPassword" type="password" placeholder="Confirm password">
+                <input class="form-control" id="exampleConfirmPassword" name="cpassword" type="password" placeholder="Confirm password">
               </div>
             </div>
           </div>
@@ -63,6 +63,36 @@
     </div>
 
   </div>
+  <?php
+			if(isset($_POST["submit"]))
+	{
+			//$res=0;
+			$email = $_POST['email'];
+      $Role = $_POST['Role'];
+     $sql = "select * from user where email ='$_POST[email]'";
+     $result = mysqli_query($con,$sql);
+		 echo mysqli_num_rows($result);
+     if(mysqli_num_rows($result)>=1)
+        {
+					?>
+					<script type="text/javascript">
+				 window.location="register.php";
+				 alert('Username exists');
+				 </script>
+        <?php }
+			else{
+			$sql2="insert into user (FirstName,LastName,,email,password) values ('','$_POST[firstname]',$_POST[lastname],'$_POST[email]','$_POST[password]',$Role)";
+			$ress=mysqli_query($con, $sql2);
+			  ?>
+			  <script type="text/javascript">
+			  window.location="login_.php";
+				alert('Registration successfull');
+			  </script>
+			  <?php
+			}
+		}
+
+	?>
   <!-- Bootstrap core JavaScript-->
   <?php
       include('include/footer.php');
