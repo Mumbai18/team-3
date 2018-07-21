625038157037e5a7d00e07d0cfcf4b5b9c308c49
Hasti <hastimehta1997@gmail.com>
2018-07-21 19:34:43 +0530

    Minor changes

--- startbootstrap-sb-admin-gh-pages/connection.php
+++ startbootstrap-sb-admin-gh-pages/connection.php
@@ -1,4 +1,13 @@
 <?php
-
-	$con=mysqli_connect("localhost","root","","jsg") or die(mysqli_error($con));
+define("SERVER","54.169.150.246"); //defines constants
+define("USERNAME","Hasti");
+define("PASSWORD", "hasti");
+define("DB","team_3");
+//create a connection
+$connection = mysqli_connect(SERVER,USERNAME,PASSWORD,DB);
+//check the connection
+if(!$connection){
+    die("Connection failed ".mysqli_connect_error());
+}
+echo "Connection successfull";
 ?>
