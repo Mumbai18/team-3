21d4e32538549ed22afa5eb762eb1507e8338d16
dhaneshm064 <33578985+dhaneshm064@users.noreply.github.com>
2018-07-21 16:14:28 +0530

    .php conversion

--- startbootstrap-sb-admin-gh-pages/registeruser.php
+++ startbootstrap-sb-admin-gh-pages/registeruser.php
@@ -1,20 +1,8 @@
 <!DOCTYPE html>
+<?php
+    include('include/header.php');
+?>
 <html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <title>SB Admin - Start Bootstrap Template</title>
-  <!-- Bootstrap core CSS-->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Custom fonts for this template-->
-  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-  <!-- Custom styles for this template-->
-  <link href="css/sb-admin.css" rel="stylesheet">
-</head>
 
 <body class="bg-dark">
   <div class="container">
@@ -47,7 +35,7 @@
             </div>
           </div>
           <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
+            <label for="exampleInputEmail1">Phone</label>
             <input class="form-control" name="phone" id="exampleInputEmail1" type="text" aria-describedby="emailHelp" placeholder="Enter email">
           </div>
           <div class="form-group">
