bd23c940b70d00c0451374e285d243bd0cce0ddf
dhaneshm064 <33578985+dhaneshm064@users.noreply.github.com>
2018-07-21 18:48:11 +0530

    login redirection

--- startbootstrap-sb-admin-gh-pages/login.php
+++ startbootstrap-sb-admin-gh-pages/login.php
@@ -3,16 +3,17 @@
 
 <?php
     include('include/header.php');
+    include('connection.php');
 ?>
 <body class="bg-dark">
 <div class="container">
     <div class="card card-login mx-auto mt-5">
         <div class="card-header">Login</div>
         <div class="card-body">
-            <form>
+            <form  class="login-form" action="#" method="post">
                 <div class="form-group">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input class="form-control" id="exampleInputEmail1" type="email" aria-describedby="emailHelp" placeholder="Enter email">
+                    <input class="form-control" name="email" id="exampleInputEmail1" type="email" aria-describedby="emailHelp" placeholder="Enter email">
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Password</label>
@@ -24,7 +25,7 @@
                             <input class="form-check-input" type="checkbox"> Remember Password</label>
                     </div>
                 </div>
-                <a class="btn btn-primary btn-block" href="index.html">Login</a>
+                 <button name="submit" style="height:50px;width:200px;background-color:blue; color:white;">login</button>
             </form>
             <div class="text-center">
                 <a class="d-block small mt-3" href="register.html">Register an Account</a>
@@ -34,8 +35,38 @@
     </div>
 </div>
 <?php
+if(isset($_POST["submit"]))
+	{
+			//$res=0;
+			$email = $_POST['email'];
+
+     $sql = "select * from user where email ='$email' and role = 'Donor'";
+     $result = mysqli_query($con,$sql);
+		 //echo mysqli_num_rows($result);
+     if(mysqli_num_rows($result)>=1)
+        {
+					?>
+					<script type="text/javascript">
+				 window.location="donor.php";
+				 </script>
+        <?php }
+
+        $sql2 = "select * from user where email ='$email' and role = 'Volunteer'";
+        $result2 = mysqli_query($con,$sql2);
+   		 echo mysqli_num_rows($result);
+        if(mysqli_num_rows($result)>=1)
+           {
+   					?>
+   					<script type="text/javascript">
+   				 window.location="donor.php";
+   				 </script>
+           <?php }
+
+
+
+
+}
     include('include/footer.php');
 ?>
 </body>
-
 </html>
