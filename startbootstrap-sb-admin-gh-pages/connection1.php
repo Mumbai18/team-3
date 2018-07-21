1bee1e515922f480cc46f335d69b0f52eae4b926
Hasti <hastimehta1997@gmail.com>
2018-07-21 21:54:02 +0530

    Connection commit

---
+++ startbootstrap-sb-admin-gh-pages/connection1.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "54.169.150.246";
+$username = "root";
+$password = "pass@123";
+$db="team_3";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected successfully";
+?>
