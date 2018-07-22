599db06ebc88b74f54f7cf560b21cac8470d381a
Hasti <hastimehta1997@gmail.com>
2018-07-22 05:51:53 +0530

    core commit

--- startbootstrap-sb-admin-gh-pages/core_dashboard.php
+++ startbootstrap-sb-admin-gh-pages/core_dashboard.php
@@ -120,32 +120,19 @@ if(isset($_POST['allocate_money'])){
 <body class="fixed-nav sticky-footer bg-dark" id="page-top">
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    <a class="navbar-brand" href="index.html">Jain Social Group EduCon Committee</a>
+    <a class="navbar-brand" href="#">Jain Social Group EduCon Committee</a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
-            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
-                <a class="nav-link" href="#">
-                    <i class="fa fa-fw fa-dashboard"></i>
-                    <span class="nav-link-text">Student Unapproved List</span>
-                </a>
-            </li>
             <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Charts">
                 <a class="nav-link" href="#">
                     <i class="fa fa-fw fa-area-chart"></i>
-                    <span class="nav-link-text">Student Approved List</span>
+                    <span class="nav-link-text">Student List</span>
                 </a>
             </li>
 
-
-            <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Menu Levels">
-                <a class="nav-link" href="#">
-                    <i class="fa fa-fw fa-table"></i>
-                    <span class="nav-link-text">Student Profiles</span>
-                </a>
-            </li>
 
             <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Menu Levels">
                 <a class="nav-link" href="#">
